6f6c906a2d9bd7324d38330ef6f93618cf1e111a
Anh Duc Cap <anhduccap@gmail.com>
2022-06-07 11:15:28 +0700

    Add feature: Create server
    Create server with MQTT client

---
+++ public/main.js
@@ -0,0 +1,63 @@
+const socket = io('http://localhost:3000');
+
+socket.on('Server-send-sensor-data', (data) => {
+    $('#humidity').html(`
+        <span>${data}lux</span>
+    `);
+});
+
+$(document).ready(() => {
+    //Control led 1
+    $('.led1Btn').click(() =>{
+        if($('.led1Btn').attr('data-click-state') == 0){
+            $('.led1Btn').attr('data-click-state', 1);
+            $('.led1Btn__left').css('background', 'transparent');
+            $('.led1Btn__right').css('background', 'rgb(0, 230, 88)');
+            $.ajax({
+                url: 'http://localhost:3000/led1?ledState=1',
+                type: 'post',
+                success: () => {
+                    console.log('Success')
+                }
+            });
+        } else {
+            $('.led1Btn').attr('data-click-state', 0);
+            $('.led1Btn__left').css('background', 'rgb(255, 17, 17)');
+            $('.led1Btn__right').css('background', 'transparent');
+            $.ajax({
+                url: 'http://localhost:3000/led1?ledState=0',
+                type: 'post',
+                success: () => {
+                    console.log('Success')
+                }
+            });
+        }
+    });
+
+    //Control led 2
+    $('.led2Btn').click(() =>{
+        if($('.led2Btn').attr('data-click-state') == 0){
+            $('.led2Btn').attr('data-click-state', 1);
+            $('.led2Btn__left').css('background', 'transparent');
+            $('.led2Btn__right').css('background', 'rgb(0, 230, 88)');
+            $.ajax({
+                url: 'http://localhost:3000/led2?ledState=1',
+                type: 'post',
+                success: () => {
+                    console.log('Success')
+                }
+            });
+        } else {
+            $('.led2Btn').attr('data-click-state', 0);
+            $('.led2Btn__left').css('background', 'rgb(255, 17, 17)');
+            $('.led2Btn__right').css('background', 'transparent');
+            $.ajax({
+                url: 'http://localhost:3000/led2?ledState=0',
+                type: 'post',
+                success: () => {
+                    console.log('Success')
+                }
+            });
+        }
+    });
+});
---
+++ public/pub_sub_client.js
@@ -0,0 +1,43 @@
+const mqtt = require("mqtt");
+
+exports.clientSub = (topic) => {
+    options = {
+        // Clean session
+      clean: true,
+      connectTimeout: 4000,
+      // Auth
+      clientId: 'NodeJS_Client_2',
+      username: 'anhduccap',
+      password: 'Duc_password1205',
+    };
+    
+    const client = mqtt.connect("http://192.168.0.106:1883", options);
+    
+    client.on("connect",() => {
+        client.subscribe(topic);
+        console.log("Client subscribed ");
+    });
+    
+    client.on("message",function(topic, message){
+        console.log(message.toString());
+    });
+}
+
+exports.clientPub = (topic, message) => {
+    options = {
+        clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+        encoding: 'utf8',
+        clean: true,
+        connectTimeout: 4000,
+        clientId: 'NodeJS_Client_1',
+        username: 'anhduccap',
+        password: 'Duc_password1205',
+    };
+    
+    const client = mqtt.connect("http://192.168.0.106:1883", options);
+    
+    client.on("connect",() => {
+        client.publish(topic,message);
+        client.end();
+    });
+}
